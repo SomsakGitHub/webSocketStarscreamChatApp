917d67d2acb5b45c4e17555ba35fffd0fc5f18ee
somsak <Somsak@uobkayhian.co.th>
2021-07-28 10:55:12 +0700

    First commit.

---
+++ starscreamSwift/Pods/Target Support Files/Pods-starscreamSwift/Pods-starscreamSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_starscreamSwift : NSObject
+@end
+@implementation PodsDummy_Pods_starscreamSwift
+@end
---
+++ starscreamSwift/starscreamSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  starscreamSwift
+//
+//  Created by ITD02 on 28/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
